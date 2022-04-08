b6571b65816c4c8a8c52cc6ca3f316907cfaf2af
Md Mahbub Alam <mahbub.alam.sobuz@gmail.com>
2022-04-08 15:26:36 +0600

    sending project for testing

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ const code = document.querySelector(".code");
 const btn = document.querySelector(".btn");
 
 let generateCode =
-  "A SEGMENT PERA PUBLIC 'CODE' \n ASSUME CS:A \n ORG 1000H \n MOV AL,00H \n OUT 1CH,AL \n MOV AL,0FFH \n OUT 18H,AL ;for red right \n OUT 1AH,AL ;for yellow light \nS : \n ";
+  "A SEGMENT PARA PUBLIC 'CODE'\n ASSUME CS A \n ORG 1000H \n S:\n MOV AL,80H\n OUT 1EH,AL ;CR \n B:\n MOV AL,00H\n OUT 1CH,AL ;PORT C\n \n MOV AL,0FFH \n OUT 18H,AL ;PORT A \n OUT 1BH,AL ;PORT B\n\n ";
 
+const generateCodeForRow = (val) => {
+  let v = `;---------\nMOV AL,${val.a}H \nOUT 18H,AL ; PORT A OFF   \n\n MOV AL,${val.b}H ; RED LIGHT\nOUT 1AH,AL ;PORT B \n\nMOV AL,${val.c}H\nOUT 1CH,AL ;PORT C\n MOV AL,00H\nOUT 1CH,AL ;PORT C TURN OFF \n`;
+  return v;
+};
 let clickedBtns = [
   [false, false, false, false, false, false, false, false],
   [false, false, false, false, false, false, false, false],
@@ -26,59 +30,69 @@ const preload = () => {
       );
     }
     col.push("<br/>");
-    values.push(col);
+    values.push(col.join(""));
     col = [];
   }
 
-  display.innerHTML = values;
+  display.innerHTML = values.join("");
 };
 
 function click1(i, j) {
   let element = document.getElementById(`btn${i},${j}`);
 
   element.classList.toggle("active");
-  clickedBtns[i][j] = element.classList.contains("active");
+
+  if (element.classList.contains("active")) {
+    clickedBtns[i][j] = 1;
+  } else clickedBtns[i][j] = 0;
 
   return clickedBtns[i][j];
 }
 
-const valGen = (i, j) => {
-  let tempB = "";
-  let tempC = [];
-  for (let k = 0; k < 8; k++) {
-    if (clickedBtns[i][k] === true) {
-      tempB += "0";
-    } else tempB += "1";
-
-    if (clickedBtns[k][j] === true) {
-      tempC.push("1");
-    } else tempC.push("0");
-  }
-  tempC = tempC.reverse().join("");
-  console.log(tempC);
-
+const valGen = (tempB, tempC) => {
   let fromBase = 2;
   let toBase = 16;
-  a = "0FF";
-  b = parseInt(tempB, fromBase).toString(toBase).padStart(2, "0");
-  c = parseInt(tempC, fromBase).toString(toBase).padStart(2, "0");
+  let b = "0";
+  let a = "0FF";
+  b += parseInt(tempB.join("").toString(), fromBase)
+    .toString(toBase)
+    .padStart(2, "0")
+    .toUpperCase();
+  let c = parseInt(tempC.reverse().join("").toString(), fromBase)
+    .toString(toBase)
+    .padStart(2, "0")
+    .toUpperCase();
+
+  console.log(a, b, c);
   return { a, b, c };
 };
 
 const generate = () => {
-  /*check which buttons are clicked */
-  for (let i = 0; i < clickedBtns.length; i++) {
-    for (let j = 0; j < clickedBtns[i].length; j++) {
-      if (clickedBtns[i][j]) {
-        let temp = valGen(i, j);
-        console.log(temp);
-        generateCode += `;---------------FOR LED ${i}${j}`;
+  let found = false;
 
-        generateCode += `MOV AL,0FFH ; VALUE FOR PORT  'A'`;
-        generateCode += `OUT 18H,AL ; SEND VALUE TO PORT 'A'`;
+  /*check which buttons are clicked */
+  for (let i = 0; i < 8; i++) {
+    let found = false;
+    let tempB = [1, 1, 1, 1, 1, 1, 1, 1];
+    let tempC = [];
+    for (let j = 0; j < 8; j++) {
+      if (clickedBtns[i][j]) {
+        tempC.push(1);
+        tempB[i] = 0;
+        found = true;
+      } else {
+        tempC.push(0);
       }
     }
+
+    if (found) {
+      let val = valGen(tempB, tempC);
+
+      generateCode += generateCodeForRow(val);
+    }
   }
+  generateCode += `;----------\nJMP C\nA ENDS\nEND`;
+  code.innerText = generateCode;
 };
 
 preload();
